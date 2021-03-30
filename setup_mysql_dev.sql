8db4d91e33671fd6299f978a350b27b6448ea8b4
root <root@DESKTOP-EIB5D01.localdomain>
2021-03-30 11:17:55 -0500

    update task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,7 @@
 -- Script that prepares a MySQL server for the project
 -- database hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+USE hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev-pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
