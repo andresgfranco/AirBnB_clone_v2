ca7c9136832bcaa61d291ed119dec7dd852454b6
andresgfranco <andresgfranco0@gmail.com>
2021-05-02 22:32:45 -0500

    Correction on class init attirbutes

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from models import storage
 import uuid
 import models
 from datetime import datetime
@@ -18,21 +19,30 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        from models import storage
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs:
-            if 'update_at' in kwargs:
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m\
-                                                            -%dT%H:%M:%S.%f')
-            if 'created_at' in kwargs:
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m\
-                                                            -%dT%H:%M:%S.%f')
-            if '__class__' in kwargs:
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+        else:
+            if 'updated_at' in kwargs.keys():
+                kwargs['updated_at'] = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'updated_at': datetime.now()})
+
+            if 'created_at' in kwargs.keys():
+                kwargs['created_at'] = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                kwargs.update({'created_at': datetime.now()})
+
+            if 'id' not in kwargs.keys():
+                kwargs.update({'id': str(uuid.uuid4())})
+
+            if '__class__' in kwargs.keys():
                 del kwargs['__class__']
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -51,8 +61,18 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                           (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary["__class__"] = str(type(self).__name__)
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+
+        if '_sa_instance_state' in dictionary.keys():
+            del dictionary['_sa_instance_state']
+
         return dictionary
+
+    def delete(self):
+        """public delete method, to delete the current instance from
+        the storage (models.storage) by calling the method delete.
+        """
+        from models import storage
+        storage.delete(self)
