bdb55db375d3999a6b3ecf37755d3c13d2aa2e20
andresgfranco <andresgfranco0@gmail.com>
2021-05-02 19:50:09 -0500

    Correction on pep8 style

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-  """Function that displays Hello HBNB!"""
+    """Function that displays Hello HBNB!"""
 
-  return "Hello HBNB!"
+    return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port="5000")
